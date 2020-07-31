180f3b9f7d4a77d63a337904c8eb854af2aaecac
Adolfo Builes <builes.adolfo@gmail.com>
2020-07-31 17:16:30 -0500

    Update transaction related end-points

--- src/steps/send/post_transactions.js
+++ src/steps/send/post_transactions.js
@@ -1,6 +1,6 @@
 module.exports = {
   instruction: "POST relevent field info to create a new payment",
-  action: "POST /send (SEP-0031)",
+  action: "POST /transactions (SEP-0031)",
   execute: async function(state, { request, response, instruction, expect }) {
     const send_server = state.send_server;
     const body = {
@@ -10,8 +10,8 @@ module.exports = {
       asset_code: state.asset_code,
       amount: state.all_field_values.amount.amount,
     };
-    request("POST /send", body);
-    const resp = await fetch(`${send_server}/send`, {
+    request("POST /transactions", body);
+    const resp = await fetch(`${send_server}/transactions`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${state.token}`,
@@ -22,13 +22,16 @@ module.exports = {
 
     expect(
       resp.status === 200,
-      `POST /send responded with status ${resp.status}`,
+      `POST /transactions responded with status ${resp.status}`,
     );
     const result = await resp.json();
-    response("POST /send", result);
+    response("POST /transactions", result);
     ["id", "stellar_account_id", "stellar_memo_type", "stellar_memo"].forEach(
       (key) => {
-        expect(result[key], `POST /send response missing property ${key}`);
+        expect(
+          result[key],
+          `POST /transactions response missing property ${key}`,
+        );
       },
     );
     state.send_memo_type = result["stellar_memo_type"];
