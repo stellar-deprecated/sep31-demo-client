1d45c2d8aa2d608ac5a6dbe7ced05e8200295dcd
Jake Urban <jake@stellar.org>
2021-02-24 14:29:19 -0800

    support SEP-31's new /info schema (sep12 object)

--- src/steps/send/check_info.js
+++ src/steps/send/check_info.js
@@ -28,15 +28,63 @@ module.exports = {
       prop(result, ["receive", state.asset_code, "fields", "transaction"]),
       "No transaction fields specified",
     );
-    state.fields = result.receive[state.asset_code].fields;
-    state.sender_sep12_type =
-      result.receive[state.asset_code].sender_sep12_type;
-    state.receiver_sep12_type =
-      result.receive[state.asset_code].receiver_sep12_type;
+    asset_info = result.receive[state.asset_code];
+    state.fields = asset_info.fields;
+    if (asset_info.sep12) {
+      if (asset_info.sep12.sender.types) {
+        let sender_types = Object.keys(asset_info.sep12.sender.types);
+        state.sender_sep12_type = sender_types[0];
+        instruction(
+          `Found the following customer types for senders: ${sender_types.join(
+            ", ",
+          )}`,
+        );
+        instruction(
+          `Using ${sender_types[0]}: ${
+            asset_info.sep12.sender.types[sender_types[0]].description
+          }`,
+        );
+      } else {
+        instruction("The anchor does not require KYC for sending customers");
+      }
+      if (asset_info.sep12.receiver.types) {
+        let receiver_types = Object.keys(asset_info.sep12.receiver.types);
+        state.receiver_sep12_type = receiver_types[0];
+        instruction(
+          `Found the following customer types for receivers: ${receiver_types.join(
+            ", ",
+          )}`,
+        );
+        instruction(
+          `Using ${receiver_types[0]}: ${
+            asset_info.sep12.receiver.types[receiver_types[0]].description
+          }`,
+        );
+      } else {
+        instruction("The anchor does not require KYC for receiving customers");
+      }
+    } else {
+      if (asset_info.sender_sep12_type) {
+        state.sender_sep12_type = asset_info.sender_sep12_type;
+        instruction(
+          `Using ${state.sender_sep12_type} type for sending customer`,
+        );
+      } else {
+        instruction("The anchor does not require KYC for sending customers");
+      }
+      if (asset_info.receiver_sep12_type) {
+        state.receiver_sep12_type = asset_info.receiver_sep12_type;
+        instruction(
+          `Using ${state.receiver_sep12_type} type for receiving customer`,
+        );
+      } else {
+        instruction("The anchor does not require KYC for receiving customers");
+      }
+    }
 
     instruction("Send is enabled for asset " + state.asset_code);
     response(
-      "The receiving anchor requires the following fields",
+      "The receiving anchor requires the following transaction fields",
       state.fields,
     );
   },
